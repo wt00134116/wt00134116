aeea89db3eed0b00366fd57aa3015ce67cd10ba8
wt00134116 <93109587+wt00134116@users.noreply.github.com>
2022-02-16 15:03:20 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,4 +1,5 @@
 <?php
+/*
 $access_token = 'cy8uWvKZ+vrFXXgLPWLDGYJHVb7M2V7t3GihlsWJlDrh7W+y9rK3P/x0Dt4iQcYOEwSi6MT60XgoS7+GffKaZiGiW9Up9ufD2oOL/3XVjBcZqxhI8voNmpzDpmH0KeE0Ern+ByTJz/c5DfQQqDlaZgdB04t89/1O/w1cDnyilFU=';
 $url = 'https://api.line.me/v2/oauth/verify';
 $headers = array('Authorization: Bearer ' .$access_token);
@@ -11,4 +12,8 @@ $result = curl_exec($ch);
 curl_close($ch);
 
 echo $result;
+*/
+curl -X GET \
+-H 'Authorization: Bearer {cy8uWvKZ+vrFXXgLPWLDGYJHVb7M2V7t3GihlsWJlDrh7W+y9rK3P/x0Dt4iQcYOEwSi6MT60XgoS7+GffKaZiGiW9Up9ufD2oOL/3XVjBcZqxhI8voNmpzDpmH0KeE0Ern+ByTJz/c5DfQQqDlaZgdB04t89/1O/w1cDnyilFU=}' \
+https://api.line.me/v1/oauth/verify
 ?>
