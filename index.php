6830e414d73a1d8912196c5a6e26f728a8ff3fe1
wt00134116 <93109587+wt00134116@users.noreply.github.com>
2022-02-16 14:58:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 //Keys to Access
 $access_token = 'cy8uWvKZ+vrFXXgLPWLDGYJHVb7M2V7t3GihlsWJlDrh7W+y9rK3P/x0Dt4iQcYOEwSi6MT60XgoS7+GffKaZiGiW9Up9ufD2oOL/3XVjBcZqxhI8voNmpzDpmH0KeE0Ern+ByTJz/c5DfQQqDlaZgdB04t89/1O/w1cDnyilFU=';
 $content = file_get_contents('php://input');
+//Parse Json
 $arr_json = json_decode($content, true);
 $strurl = "https://api.line.me/v2/bot/message/reply";
 
@@ -10,16 +11,20 @@ $arrhdr = array();
 $arrhdr[] = "Content-Type: application/json";
 $arrhdr[] = "Authorization: Bearer {$Access_Token}";
 
-//Condition to Send Message
-if($arr_json['events'][0]['message']['text'] == "สวัสดีครับ" || 
-   $arr_json['events'][0]['message']['text'] == "สวัสดีค่ะ" ||
-   $arr_json['events'][0]['message']['text'] == "ว่าไง")
-{
-  $arrpostdata = array();
-  $arrpostdata['replytoken'][0]['type'] = "text";
-  $arrpostdata['message'][0]['text'] = "สวัสดีครับ มีอะไรให้รับใช้ครับ";
-  $arrPostData['messages'][0]['originalContentUrl'] = 'https://github.com/swunews/LINE-BOT-PHP-Starter/blob/master/grief-and-loss.jpg?raw=true';
-  $arrPostData['messages'][0]['previewImageUrl'] = 'https://github.com/swunews/LINE-BOT-PHP-Starter/blob/master/grief-and-loss_preview.jpg?raw=true';
+if(!is_null($arr_json['events'])){
+   foreach($arr_json['events'] as $arr_json){
+      //Condition to Send Message
+      if($arr_json['events'][0]['message']['text'] == "สวัสดีครับ" || 
+         $arr_json['events'][0]['message']['text'] == "สวัสดีค่ะ" ||
+         $arr_json['events'][0]['message']['text'] == "ว่าไง")
+      {
+        $arrpostdata = array();
+        $arrpostdata['replytoken'][0]['type'] = "text";
+        $arrpostdata['message'][0]['text'] = "สวัสดีครับ มีอะไรให้รับใช้ครับ";
+        //$arrPostData['messages'][0]['originalContentUrl'] = 'https://github.com/swunews/LINE-BOT-PHP-Starter/blob/master/grief-and-loss.jpg?raw=true';
+        //$arrPostData['messages'][0]['previewImageUrl'] = 'https://github.com/swunews/LINE-BOT-PHP-Starter/blob/master/grief-and-loss_preview.jpg?raw=true';
+      }
+   }
 }
 
 //Post
@@ -33,4 +38,7 @@ curl_setopt($ch, curlopt_returntransfer, true);
 curl_setopt($ch, curlopt_ssl_verifypeer, false);
 $result = curl_exec($ch);
 curl_close($ch);
+
+echo $result. \r\n;
+echo "OK";
 ?>
